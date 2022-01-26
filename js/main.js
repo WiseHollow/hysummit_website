ea628647c6c8ead018fa5a19940481476eb11702
BuildTools <unconfigured@null.spigotmc.org>
2022-01-26 09:56:54 -0600

    Make player count fetch optional.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // If you don't want the particles, change the following to false
 const doParticles = false;
-
+// If you don't want player count fetched, set to false
+const fetchPlayerCount = false;
 
 
 
@@ -55,15 +56,17 @@ $(document).on("click", ".ip", () => {
 
 // This is to fetch the player count
 $(document).ready(() => {
-    let ip = $(".sip").attr("data-ip");
-    let port = $(".sip").attr("data-port");
-    if (port == "" || port == null) port = "25565";
-    if (ip == "" || ip == null) return console.error("Error fetching player count - is the IP set correctly in the HTML?");
-    updatePlayercount(ip, port);
-    // Updates every minute (not worth changing due to API cache)
-    setInterval(() => {
+    if (fetchPlayerCount) {
+        let ip = $(".sip").attr("data-ip");
+        let port = $(".sip").attr("data-port");
+        if (port == "" || port == null) port = "25565";
+        if (ip == "" || ip == null) return console.error("Error fetching player count - is the IP set correctly in the HTML?");
         updatePlayercount(ip, port);
-    }, 60000);
+        // Updates every minute (not worth changing due to API cache)
+        setInterval(() => {
+            updatePlayercount(ip, port);
+        }, 60000);
+    }
 });
 
 const updatePlayercount = (ip, port) => {

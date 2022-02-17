8deb5bc373361805f0dc3c6c3236ad9f941fa7f9
BuildTools <unconfigured@null.spigotmc.org>
2022-02-17 10:35:33 -0600

    Added better modal support with custom JS. Added features modal.

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,35 @@
-// Get the modal
-var modal = document.getElementById("ranksModal");
+const modalButtons = $("a[data-js-toggle='modal']");
+modalButtons.each(function() {
+  const button = $(this);
+  const dataTarget = $(this).attr('data-js-target');
+  if (dataTarget) {
+    const modalTargets = $(dataTarget);
+    if (modalTargets.length == 0) {
+      console.log('data-target not found: ' + dataTarget);
+      return;
+    }
 
-// Get the button that opens the modal
-var btn = document.getElementById("ranksBtn");
+    const modalTarget = modalTargets[0];
+    const closeButton = $(modalTarget).find('.close');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // modalTarget.onclick = function() {
+    //   console.log('click');
+    // };
+    button.on('click', function() {
+      modalTarget.style.display = "block";
+    });
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    closeButton.on('click', function() {
+      modalTarget.style.display = "none";
+    });
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    window.addEventListener('click', (event) => {
+      if (event.target == modalTarget) {
+        modalTarget.style.display = "none";
+      }
+    })
+  } else {
+    console.log("'data-target' not set within modal.");
+    return;
   }
-}
+})
